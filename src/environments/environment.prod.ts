9e36814ab5ba72ce902ee2d797edebcfdb9ec0ee
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-13 12:29:37 -0500

    Actualización de ip para api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://13.59.171.93:8000/api/'
+  apiUrl: 'http://3.13.185.75:8000/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000/api/'
+  apiUrl: 'http://3.13.185.75:8000/api/'
 };
 
 /*
