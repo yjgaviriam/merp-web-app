5cc588308eb78e0c11447cf2405b6e177f6a9934
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-17 00:36:47 -0500

    Testing CI

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -2,4 +2,6 @@
 #Get servers list
 set -f
 echo "Deploy project on server $DEPLOY_SERVER"
+USER_GIT=$USER_GIT
+USER_GIT_PASSWORD=$USER_GIT_PASSWORD
 ssh -o StrictHostKeyChecking=no -T "$USER_SERVER@$DEPLOY_SERVER" 'bash -s' < ./deploy-production.sh
