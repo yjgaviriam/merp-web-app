195c12af793caa1c3911d98398890241b7bee063
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-17 00:40:40 -0500

    Testing Ci Deploy

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -4,4 +4,4 @@ set -f
 echo "Deploy project on server $DEPLOY_SERVER"
 USER_GIT=$USER_GIT
 USER_GIT_PASSWORD=$USER_GIT_PASSWORD
-ssh -o StrictHostKeyChecking=no -T "$USER_SERVER@$DEPLOY_SERVER" 'bash -s' < ./deploy-production.sh
+ssh -o StrictHostKeyChecking=no -T "$USER_SERVER@$DEPLOY_SERVER" "cd /home/ubuntu/merp-web-app/ && git pull https://${USER_GIT}:${USER_GIT_PASSWORD}@gitlab.com/yjgaviriam/merp-web-app.git"
