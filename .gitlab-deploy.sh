a6c98990028a1ed1ed362c930b1171ec12667097
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-16 21:10:26 -0500

    Testing Ci Deploy

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -5,7 +5,7 @@ set -f
 string=$DEPLOY_SERVER
 array=(${string//,/ })
 #Iterate servers for deploy and pull last commit
-for i in "${!array[@]}"do
+for i in "${!array[@]}" do
     echo "Deploy project on server ${array[i]}"
     ssh ubuntu@${array[i]} "ls /var/www"
 done
