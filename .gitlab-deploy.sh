862275b6babe47593b41532d646cfa5e8b7be8b9
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-16 23:04:38 -0500

    Testing Ci Deploy

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
 #Get servers list
 set -f
-ssh ubuntu@$DEPLOY_SERVER "ls /var/www"
+string=$DEPLOY_SERVER
+array=(${string//,/ })
+#Iterate servers for deploy and pull last commit
+for i in "${!array[@]}" do
+    echo "Deploy project on server ${array[i]}"
+    ssh ubuntu@${array[i]} "cd /var/www && git pull origin master"
+done
