e400b0ff562c8f0da169bb96ec3be677d298eba2
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-16 23:38:01 -0500

    Testing CI

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -2,5 +2,6 @@
 #Get servers list
 set -f
 echo "Deploy project on server $DEPLOY_SERVER"
-bash ./deploy/restart-server-production.sh
+# bash ./deploy/restart-server-production.sh
 # ssh -o "StrictHostKeyChecking=no" -p22 ubuntu@$DEPLOY_SERVER "mkdir /var/www/html_tmp"
+echo "Finish-------------------------"
