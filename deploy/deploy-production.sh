5648d5c8bf7158b672f0d66951351ab4e8f24807
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-16 22:59:42 -0500

    Testing CI

--- deploy/deploy-production.sh
+++ deploy/deploy-production.sh
@@ -8,25 +8,9 @@ set -e
 # echo -e "${SSH_PRIVATE_KEY}" >> ./ubuntu_key.pem
 # chmod 400 ./ubuntu_key.pem
 
-# Lets write the public key of our aws instance
-eval $(ssh-agent -s)
-# ssh-keyscan -t rsa ${DEPLOY_SERVER} >> ~/.ssh/known_hosts
-ssh-add <(echo "$SSH_PRIVATE_KEY")
-
-# ** Alternative approach
-# echo -e "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
-# chmod 600 /root/.ssh/id_rsa
-# ** End of alternative approach
-
-# disable the host key checking.
-# bash ./deploy/disable-host-key-checking.sh
-mkdir -p ~/.ssh
-touch ~/.ssh/config
-'[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config'
-
 DEPLOY_SERVER=$DEPLOY_SERVER
 
-echo "a"
+echo "a ----------------------------------------------"
 
 echo "deploying to ${DEPLOY_SERVER}"
 ssh ubuntu@${DEPLOY_SERVER} 'bash -s' < ./deploy/restart-server-production.sh
