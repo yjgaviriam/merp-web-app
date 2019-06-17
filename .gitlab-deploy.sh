aeb729b236f94cc01d482d0b5da1a58072eb5b01
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-17 00:44:40 -0500

    Testing CI

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -4,4 +4,6 @@ set -f
 echo "Deploy project on server $DEPLOY_SERVER"
 USER_GIT=$USER_GIT
 USER_GIT_PASSWORD=$USER_GIT_PASSWORD
-ssh -o StrictHostKeyChecking=no -T "$USER_SERVER@$DEPLOY_SERVER" "cd /home/ubuntu/merp-web-app/ && git pull https://${USER_GIT}:${USER_GIT_PASSWORD}@gitlab.com/yjgaviriam/merp-web-app.git"
+ssh -o StrictHostKeyChecking=no -T "$USER_SERVER@$DEPLOY_SERVER" "cd /home/ubuntu/merp-web-app/ &&
+git pull https://${USER_GIT}:${USER_GIT_PASSWORD}@gitlab.com/yjgaviriam/merp-web-app.git && 
+npm install && ng build --prod && sudo cp -a ./dist/merp-web-app/. /var/www/html/"
