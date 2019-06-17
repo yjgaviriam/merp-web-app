28d56d2691255b70bdb4636a022b4247e4207583
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-16 23:18:18 -0500

    Testing Ci Deploy

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -2,4 +2,4 @@
 #Get servers list
 set -f
 echo "Deploy project on server $DEPLOY_SERVER"
-ssh ubuntu@$DEPLOY_SERVER "cd /var/www && git pull origin master"
+ssh -o "StrictHostKeyChecking=no" -p22 ubuntu@$DEPLOY_SERVER "mkdir /var/www/html_tmp"
