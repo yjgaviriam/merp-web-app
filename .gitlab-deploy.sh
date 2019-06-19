9c5b9bb364280d5dc6a5d902c2b76832e808c3cb
Jhonier <38549465+JhonierGaviria@users.noreply.github.com>
2019-06-18 22:40:39 -0500

    Se agrega script de sonar al despliegue

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,4 +6,4 @@ USER_GIT=$USER_GIT
 USER_GIT_PASSWORD=$USER_GIT_PASSWORD
 ssh -o StrictHostKeyChecking=no -T "$USER_SERVER@$DEPLOY_SERVER" "cd /home/ubuntu/merp-web-app/ &&
 git pull https://${USER_GIT}:${USER_GIT_PASSWORD}@gitlab.com/yjgaviriam/merp-web-app.git && 
-npm install && ng build --prod && sudo cp -a ./dist/merp-web-app/. /var/www/html/"
+sonar-scanner && npm install && ng build --prod && sudo cp -a ./dist/merp-web-app/. /var/www/html/"
